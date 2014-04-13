ca4c6ee49a470ebf753d3952864b9857b9284972
Kieran Ball <kball369@gmail.com>
2014-04-13 19:11:01 +0100

    Update Gemfile.lock for Heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,19 +20,22 @@
 //= require bootstrap
 
 $(document).ready(function(){ 
-	$('div.alert').slideDown(4000)
+	$('div.generalalert').slideDown(4000)
 	setTimeout(function(){
-		$('div.alert').slideUp(1000)
-	}, 7000);
+		$('div.generalalert').slideUp(1000)
+	}, 3000);
 
 	$('.table-striped').dataTable( {
 	  "aoColumnDefs": [
 	      { "bSortable": true, "aTargets": [ 0 ] }
 	  ],
 	  "aaSorting": [[1, 'asc']]
-
-
 	});
+
+		$('div.alert').slideDown(4000)
+	setTimeout(function(){
+		$('div.alert').slideUp(1000)
+	}, 7000);
 
 })
 
